07a4f53ad05e056cb877d035e9934e68706519fb
redassser <redryan890@gmail.com>
2023-01-19 05:00:21 -0500

    temporary bots page

--- pages/projects/bots.js
+++ pages/projects/bots.js
@@ -1,27 +1,16 @@
 import Layout from '../../components/layout'
 import Sidebar from '../../components/sidebar'
 
-export default function About() {
+export default function Bots() {
   return (
     <section>
-      <h3>I spent a lot of time on these things -</h3>
-      <p>- during High School, and some time afterwards too</p>
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+      <h1 style={{textAlign:"center"}}>I started out my programming career with discord bots the summer before highschool.</h1>
+      
     </section>
-    
   )
 }
 
-About.getLayout = function getLayout(page) {
+Bots.getLayout = function getLayout(page) {
   return (
     <Layout>
       <Sidebar />
