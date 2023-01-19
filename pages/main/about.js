eb6918202c7556aba96e168948ec330a04fb7139
redassser <redryan890@gmail.com>
2023-01-19 04:28:40 -0500

    more about me

--- pages/main/about.js
+++ pages/main/about.js
@@ -1,21 +1,30 @@
 import Layout from '../../components/layout'
 import Sidebar from '../../components/sidebar'
 import Image from 'next/image'
+
 import ryan from "../../components/ryanpic.png"
+import ryansmal from "../../components/about/ryansmal.png"
 
 export default function About() {
   return (
     <section>
-      <p>
-        I'm Ryan Piedrahita, from West New York, NJ. I'm a programmer and college 
-        student who likes to talk and really likes to listen. 
+      <div style={{float:"left", margin:"10px", marginTop:"30px"}}><Image src={ryansmal}/></div>
+      <p style={{textAlign: "center"}}>
+        I'm Ryan Piedrahita, a Colombian-american from <br/> West New York (New Jersey) 
       </p>
       <p>
-         I hope to become a Software Engineer, solving problems and inventing solutions! 
-         I also like to educate, working with and teaching kids comes naturally, as well 
-         as conversation and empathy. 
+         I see software engineering as sort of the modern day blacksmithy. Using the tools of
+         the craft, I mold the flowing, featureless text and strike the molten data with
+         precision.
+      </p>
+      <p style={{textAlign: "center"}}>
+        The brave knight need not forge the steel himself to wield his weapon with grace and dignity.
       </p>
       <p>
+        I'm also pretty good with people. My friends say I have too much patience, but I just love listening to 
+        people, no matter what they're saying.
+      </p>
+      <p style={{textAlign: "center"}}>
         Ultimate goal: become the President, do good in the world, and finish this website
       </p>
     </section>
